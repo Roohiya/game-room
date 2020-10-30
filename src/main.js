18c25d544f0a162427dab83e8d9e8c70316944b3
Roohiya <roohiya.dudekula@gmail.com>
2020-10-30 16:42:25 +0800

    Allow users to create and join the game

--- src/main.js
+++ src/main.js
@@ -1,62 +1,57 @@
 import Koa from 'koa'
-// import send from 'koa-send'
-// import serve from 'koa-static'
-// import appRoot from 'app-root-path'
-// import fs from 'fs'
-// import path from 'path'
 import http from 'http'
 import socketIo from 'socket.io'
 import router from './router'
-import cors from 'koa2-cors'
+import shortId from 'shortid'
 
 const app = new Koa()
 app
 .use(router.routes())
 .use(router.allowedMethods())
 
-// app.use(serve(`${appRoot}/public`))
-
-// const template = fs.readFileSync(`${appRoot}/public/index.html`)
-
-// app.use(async (ctx) => {
-//     ctx.type = 'html'
-//     ctx.body = template
-// })
-
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   credentials: true,
-//   allowMethods: ['GET', 'POST', 'DELETE'],
-//   allowHeaders: ['Content-Type', 'Authorization', 'Accept']
-// }))
-
 const server = http.createServer(app)
-const io = socketIo(server, { origins: '*:*'})
-
-let interval
-
-io.on('connection', socket => {
-  // console.log("New client connected")
-  if (interval) {
-    clearInterval(interval)
-  }
-
-  interval = setInterval(() => getApiAndEmit(socket), 1000)
-
-  // socket.on("disconnect", () => {
-  //   console.log("Client disconnected")
-  //   clearInterval(interval)
-  // })
+const io = socketIo(server, {
+  origins: '*:*',
+  pingTimeout: 60000
 })
 
-const getApiAndEmit = socket => {
-  const response = new Date()
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response)
-}
+let games = {}
+
+io.on('connection', socket => {
+  console.log('New channel connected!')
+
+  socket.on('createGame', (data) => {
+    const { playerOneName } = data
+
+    const roomId = shortId.generate()
+    socket.join(roomId)
+
+    games[roomId] = {
+      playerOne: playerOneName,
+      room: roomId
+    }
+
+    socket.emit('newGame', games[roomId])
+  })
+
+  socket.on('joinGame', (data) => {
+    const { playerTwoName, roomId } = data
+
+    const roomNum = io.nsps['/'].adapter.rooms[roomId]
+
+    if (roomNum && roomNum.length === 1) {
+      socket.join(roomId)
+
+      games[roomId].playerTwo = playerTwoName
+      games[roomId].startGame = true
+
+      io.sockets.in(roomId).emit('connectToRoom', games[roomId])
+    } else {
+      socket.emit('connectToRoom', {startGame: false})
+    }
+  })
+})
 
 server.listen(4242, () => {
   console.log('Application is starting on port 4242')
 })
-
-// test, to remove
